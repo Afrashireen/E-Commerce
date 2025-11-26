b826b6cf411cb1271fabfbcf27650c18e6bd2888
Afrashireen <142524394+Afrashireen@users.noreply.github.com>
2025-11-26 17:40:13 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,13 +6,10 @@ export default function Home({ searchText, category }) {
   return (
     <div className="container mt-4">
       <div className="row">
-
-        {/* Center Feed */}
         <div className="col-lg-8 col-md-8 col-sm-12">
           <PostFeed selectedCategory={category} searchText={searchText} />
         </div>
 
-        {/* Right: Create Post Box */}
         <div className="col-lg-4 col-md-4 col-sm-12">
           <CreatePost onPostCreated={() => window.location.reload()} />
         </div>
